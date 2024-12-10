23716cf3ca964ec7f0c213811a70cf91609bb798
david <david.mlcoch@gmail.com>
2024-12-10 10:44:06 +0000

    add execution mode to CompletionsWrapper

--- src/sentinel/wrappers/openai.py
+++ src/sentinel/wrappers/openai.py
@@ -3,14 +3,12 @@ Wrapper for the OpenAI client to intercept requests and responses.
 """
 
 import json
-from typing import Any, Callable, List, Optional
-from uuid import UUID, uuid4
+from typing import Any, Callable, List, Optional, Dict
+from uuid import UUID
 from openai import OpenAIError
 from sentinel.api.logger import APILogger, SentinelLoggingError
 from sentinel.settings import settings
 from sentinel.registration.helper import create_run, register_project, register_task, APIClientFactory, register_tools_and_supervisors
-import inspect
-from sentinel.supervision.config import get_supervision_config, get_supervision_context
 
 class CompletionsWrapper:
     """Wraps chat completions with logging capabilities"""
@@ -18,11 +16,13 @@ class CompletionsWrapper:
         self, 
         completions: Any, 
         logger: APILogger, 
-        run_id: UUID
+        run_id: UUID,
+        execution_mode: str = "supervision"
     ):
         self._completions = completions
         self.logger = logger
         self.run_id = run_id
+        self.execution_mode = execution_mode
     
     def create(self, *args, **kwargs) -> Any:
         # Log the entire request payload
@@ -35,14 +35,21 @@ class CompletionsWrapper:
             # Make API call
             response = self._completions.create(*args, **kwargs)
 
-            # Log the entire response
-            try:
-                response_data = response if isinstance(response, dict) else response.dict()
-                request_data = kwargs
-                self.logger.log_response(response_data, request_data, self.run_id)
-            except Exception as e:
-                print(f"Warning: Failed to log response: {str(e)}")
-            
+            if self.execution_mode == "supervision":
+                # SYNC LOGGING + SUPERVISION
+                try:
+                    
+                    new_response = self.logger.log_response(response, request_kwargs=kwargs, run_id=self.run_id, execution_mode=self.execution_mode, completions=self._completions, args=args)
+                    if new_response is not None:
+                        print(f"New response: {new_response}")
+                        return new_response
+                except Exception as e:
+                    print(f"Warning: Failed to log response: {str(e)}")
+            else:
+                # ASYNC LOGGING + SUPERVISION
+                print("Async logging + supervision not implemented yet")
+                pass #TODO: Implement async logging
+                
             return response
             
         except OpenAIError as e:
@@ -54,6 +61,7 @@ class CompletionsWrapper:
 def sentinel_openai_client(
     openai_client: Any, 
     run_id: UUID,
+    execution_mode: str = "supervision"
 ) -> Any:
     """
     Wraps an OpenAI client instance with logging capabilities and registers supervisors.
@@ -69,7 +77,8 @@ def sentinel_openai_client(
         openai_client.chat.completions = CompletionsWrapper(
             openai_client.chat.completions, 
             logger,
-            run_id
+            run_id,
+            execution_mode
         )
         return openai_client
     except Exception as e:
@@ -79,8 +88,9 @@ def sentinel_init(
     project_name: str = "My Project", 
     task_name: str = "My Agent", 
     run_name: str = "My Run",
-    tools: Optional[List[Callable]] = None
-) -> None:
+    tools: Optional[List[Callable]] = None,
+    execution_settings: Dict[str, Any] = {}
+) -> UUID:
     """
     Initializes supervision for a project, task, and run.
     """
@@ -92,7 +102,7 @@ def sentinel_init(
     run_id = create_run(project_id, task_id, run_name)
     print(f"Registered new run with ID: {run_id}")
 
-    register_tools_and_supervisors(run_id, tools)
+    register_tools_and_supervisors(run_id, tools, execution_settings)
 
     return run_id
 
